aa2bc5fc6c2e340eb2f931e000e6b5592d9b8f6e
Archit Satish Joshi <joshi.archit.2904@gmail.com>
2025-10-25 22:55:48 -0500

    Initial Commit

---
+++ Bookd/BookdApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookdApp.swift
+//  Bookd
+//
+//  Created by Archit Joshi on 10/25/25.
+//
+
+import SwiftUI
+
+@main
+struct BookdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
